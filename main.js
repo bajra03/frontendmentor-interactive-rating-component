ea9d8ab53e49d5acce4befc5954029170b4ed920
bajra <bajra.idabagus@karmagroup.com>
2022-12-30 12:10:58 +0800

    add validation

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
-const btnSubmit = document.querySelector('.btn-submit');
+const btnSubmit = document.querySelector(".btn-submit");
+const stars = document.querySelectorAll("input[name=star]");
+const toastMessage = document.querySelector(".toast");
 
-btnSubmit.addEventListener('click', (e) => { 
+btnSubmit.addEventListener('click', (e) => {
   e.preventDefault();
-  const stars = document.getElementsByName('star');  
-
-  for (let i = 0; i < stars.length; i++) { 
-    if (stars[i].checked) { 
-      const starSelected = document.querySelector('.star-selected');
-      const thankyouContainer = document.querySelector('.star-thankyou-container');
-
-      starSelected.innerHTML = stars[i].value;
-      
-      document.querySelector('.loader').classList.remove('hide');
-      setTimeout(() => {
-        document.querySelector('.loader').classList.add('hide');
-        btnSubmit.parentElement.classList.add('hide');
-        thankyouContainer.classList.remove('hide');        
-      }, 1500);
+  if (validation()) {
+    for (let i = 0; i < stars.length; i++) {
+      if (stars[i].checked) {
+        const starSelected = document.querySelector('.star-selected');
+        const thankyouContainer = document.querySelector('.star-thankyou-container');
+  
+        starSelected.innerHTML = stars[i].value;
+  
+        document.querySelector('.loader').classList.remove('hide');
+        setTimeout(() => {
+          document.querySelector('.loader').classList.add('hide');
+          btnSubmit.parentElement.classList.add('hide');
+          thankyouContainer.classList.remove('hide');
+        }, 1500);
+      }
     }
+  } else {
+    toastMessage.classList.add("active");
+    setTimeout(() => {
+      toastMessage.classList.remove("active")
+    }, 3000);
   }
-})
+});
+
+function validation() { 
+  let isValid = false;
+  stars.forEach((star) => {
+    if (star.checked) {
+      isValid = true;
+    }
+  });
+  
+  return isValid;
+}
